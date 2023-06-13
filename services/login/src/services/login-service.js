9c59c3c13c769ef148c5b13ae24fc229ed822f21
Tiffany <tiffany831101@gmail.com>
2023-06-13 14:35:55 +0800

    feat: test for ci

--- services/login/src/services/login-service.js
+++ services/login/src/services/login-service.js
@@ -51,7 +51,7 @@ class LoginService {
    * generate salt and add to the pwd
    * create user
    * generate signature
-   * if signup before, pls route to signin page, not duplicate signup
+   * if signup before, pls route to signin page, no duplicate signup
    * @param { object } userInputs
    * @returns token and the userid => should be stored in the sessionStorage
    */
