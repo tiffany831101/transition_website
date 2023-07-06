4d2aced35664423b79701cf9513bac6cb7683017
Tiffany <tiffany831101@gmail.com>
2023-07-06 12:35:40 +0800

    bug: add port in config

--- services/login/src/config/index.js
+++ services/login/src/config/index.js
@@ -13,4 +13,5 @@ if (process.env.NODE_ENV.trim() !== "prod") {
 module.exports = {
   ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
   SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+  PORT: process.env.PORT,
 };
